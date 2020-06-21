e8cea0b0190f5da377e10a94749ee04bfbbf50b7
Rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2020-06-21 14:10:39 +0900

    update

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ sudo apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io

aa5a5c293d1caf3061a86014fd51bc92456a499a
rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2020-08-12 15:18:15 +0900

    Add mongo install

--- install.sh
+++ install.sh
@@ -14,3 +14,8 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
